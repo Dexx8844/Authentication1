72880f2ae01d1611a95fce4cdf320b6c3cc82a0c
Chidex <chideraaslem@gmail.com>
2025-05-29 19:57:19 +0100

    first commit

--- middleware/passport.js
+++ middleware/passport.js
@@ -7,7 +7,7 @@ const userModel = require('../model/user');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:4343/api/v1/auth/google/login"//This is where the user will be redirected after successful login
+    callbackURL: process.env.GOOGLE_CALLBACK_URL, //This is where the user will be redirected after successful login
   },
   async function(accessToken, refreshToken, profile, cb) {
     try {
--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const nodeMailer = require('nodemailer');
 
 const sendMail = async ( options )=>{``
   
     const transporter = await nodeMailer.createTransport({
         host: process.env.EMAIL_HOST,
-        service: process.env.EMAIL_SERVICE,
+        service: process.env.SERVICE,
         port: 465,
 
         secure: true, // true for port 465, false for other ports
@@ -20,7 +21,7 @@ const sendMail = async ( options )=>{``
       async function main() {
         // send mail with defined transport object
         const info = await transporter.sendMail({
-          from: `"Fred Foo 👻" <${process.env.APP.USERNAME}>`, // sender address
+          from: `" chidera" <${process.env.APP.USERNAME}>`, // sender address
           to: options.email, // list of receivers
           subject: options.subject, // Subject line
           html: options.html, // html body
@@ -31,7 +32,7 @@ const sendMail = async ( options )=>{``
 
 
       const mailOption = {
-        subject: options.subject, text:options.text, from:"obusco4lyfe@gmail.com", to: options.email, html:options.html
+        subject: options.subject, text:options.text, from:"chideraaslem@gmail.com", to: options.email, html:options.html
       };
       await transporter.sendMail(mailOption)
 
